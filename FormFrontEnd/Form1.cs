85493fdcc69c1eec6190d26d55b8241bdb398d9d
Debora Tristao <48920353+debytristao@users.noreply.github.com>
2019-05-03 08:32:40 -0300

    ReadWritePropertiesDemo1
    ReadWritePropertiesDemo1

--- FormFrontEnd/Form1.cs
+++ FormFrontEnd/Form1.cs
@@ -11,7 +11,8 @@ using System.Windows.Forms;
 namespace FormFrontEnd
 {
     public partial class Form1 : Form
-    {     
+    {        
+        Balloon myFirstBallon;
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +20,36 @@ namespace FormFrontEnd
 
         private void btnSet_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                //I must intantiate my object =)
+                myFirstBallon = new Balloon()
+                {
+                    Colour = txtColor.Text.Trim(),
+                    Diameter = Convert.ToDecimal(txtDiameter.Text),
+                    Height = Convert.ToDecimal(txtHeight.Text),
+                };
+                //myFirstBallon.Colour = txtColor.Text.Trim();
+                //myFirstBallon.Diameter = Convert.ToDecimal(txtDiameter.Text);
+                //myFirstBallon.Height = Convert.ToDecimal(txtHeight.Text);
+            }
+            catch(DataException ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString(), MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
         }
 
         private void btnGet_Click(object sender, EventArgs e)
         {
-            
+            lblOutput.Text = $"Colour: {myFirstBallon.Colour}\n" +
+            $"Height: {myFirstBallon.Height.ToString()}\n" +
+            $"Diameter: {myFirstBallon.Diameter.ToString()}";
         }
     }
 }
